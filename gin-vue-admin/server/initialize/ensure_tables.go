4519ac6035a7791022311257e9e4c17d67d06a97
sado <lize240810@163.com>
2025-09-15 00:07:01 +0800

    我的核销活动

--- gin-vue-admin/server/initialize/ensure_tables.go
+++ gin-vue-admin/server/initialize/ensure_tables.go
@@ -53,6 +53,7 @@ func (e *ensureTables) MigrateTable(ctx context.Context) (context.Context, error
 		sysModel.Condition{},
 		sysModel.JoinTemplate{},
 		sysModel.SysParams{},
+		sysModel.ActivityRegistration{}, // 添加活动报名表
 
 		adapter.CasbinRule{},
 
@@ -94,6 +95,8 @@ func (e *ensureTables) TableCreated(ctx context.Context) bool {
 		sysModel.SysExportTemplate{},
 		sysModel.Condition{},
 		sysModel.JoinTemplate{},
+		sysModel.SysParams{},
+		sysModel.ActivityRegistration{}, // 添加活动报名表
 
 		adapter.CasbinRule{},
 
